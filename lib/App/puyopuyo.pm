bd0b2154bb4ba87d181f96978eb726c990a85033
Syohei YOSHIDA <syohex@gmail.com>
2011-02-09 17:51:50 +0900

    - PODを修正しました  - それに伴い自動生成の READMEも修正されました

--- lib/App/puyopuyo.pm
+++ lib/App/puyopuyo.pm
@@ -291,36 +291,115 @@ __END__
 
 =encoding utf-8
 
-=for stopwords
-
 =head1 NAME
 
-App::puyopuyo - PuyoPuyo resolver
+App::puyopuyo - PuyoPuyo resolver implemented by perl
 
 =head1 SYNOPSIS
 
   use App::puyopuyo;
 
-  my $app = App::puyopuyo->new(width => 6, height => 13, animation => 1);
-  $app->load_puyo("rrgg\nggrr\nyybb\n");
+  my $app = App::puyopuyo->new(
+      width => 6,
+      height => 13,
+      animation => 1,
+      color => 1,
+  );
+  $app->load_puyo(" RGY\nRGYB\nRGYB\nRGYBB");
+  $app->run;
+  # Let's see puyopuyo animation
 
 =head1 DESCRIPTION
 
-App::puyopuyo is
+App::puyopuyo is PuyoPuyo resolver.
+
+=head1 INTERFACE
+
+=head2 Class Method
+
+=head3 C<< App::puyopuyo->new(%args) >>
+
+Creates and returns new PuyoPuyo instance.
+
+I<%args> might be
+
+=over
+
+=item width
+
+I<width>: width of stage. If width is not set, then dies;
+
+=item height :Int
+
+I<width>: height of stage. If height is not set, then dies;
+
+=item animation :Bool = undef
+
+I<animation>: display PuyoPuyo animation.
+
+=item color :Bool = undef
+
+I<color>: display coloring PuyoPuyo animation.
+This parameter should be set with I<animation> parameter.
+
+=item puyo :Str
+
+I<puyo>: display puyo specified character instead of
+color characters ('R', 'G', 'B', 'Y', 'C', 'M', 'O').
+R is Red, G is Green, B is Blue, Y is Yellow, C is Cyan,
+M is Magenta  and O is Ojama Puyo.
+
+This parameter should be set with I<animation> and I<color>
+parameter.
+
+=item double_space :Bool :
+
+I<double_space>: display space with double space.
+If I<puyo> is Zenkaku, you should set I<double_space> true.
+
+=back
+
+=head2 Instance Method
+
+=head3 C<< $app->load_puyo($data) >>
+
+load I<$data> of PuyoPuyo layout.
+I<$data> is might be (string|string reference|filehandle)
+
+For example, layout is like this,
+
+  YGRBYY
+  YGGRBR
+  GBBBRR
+
+then $data = "YGRBYY\nYGGRBR\nGBBBRR"
+
+=head3 C<< $app->run >>
+
+resolve PuyoPuyo. If I<animation> parameter is set, display animation.
 
 =head1 AUTHOR
 
 Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
 
-=head1 COPYRIGHT
+=head1 SEE ALSO
 
-Copyright 2011- Syohei YOSHIDA
+PuyoPuyo is proprietary from SEGA Corporation.
+
+L<http://www.sega.co.jp/>
+
+This problem is proposed by this page.
+
+http://okajima.air-nifty.com/b/2011/01/2011-ffac.html
+
+Sample program is based on Kame's movie.
+Kame is very famous PuyoPuyo Player.
 
 =head1 LICENSE
 
+Copyright 2011- Syohei YOSHIDA
+
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
-
-=head1 SEE ALSO
 
 =cut
